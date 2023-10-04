332e8efa153c1b1081ab148aff0d8afc632a8345
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-10-04 21:07:06 +0100

    Install and set up: - shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
    return(
-      <h1 className='text-3xl font-bold underline'> Hello World! </h1>
+      <Button> Hello World! </Button>
    );
 }
